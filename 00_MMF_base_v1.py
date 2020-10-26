7206f48974d7884b6f5a3b519e5d16620c280ce4
Jennifer Gottschalk <jgottschalk@masseyhigh.school.nz>
2020-10-26 16:31:57 +1300

    Up to age checker (slightly broken)

--- 00_MMF_base_v1.py
+++ 00_MMF_base_v1.py
@@ -27,9 +27,40 @@ def not_blank(question):
 # Ask user if they have used the program before & show instructions if necessary
 
 # Loop to get ticket details
+# start of loop
+
+# initialise loop so that it runs at least once
+name = ""
+count = 0
+MAX_TICKETS = 5
+
+while name != "xxx" and count < MAX_TICKETS:
+
+    # tells user how many seats are left
+    if count < 4:
+        print("You have {} seats "
+              "left".format(MAX_TICKETS - count))
+
+    # Warns user that only one seat is left!
+    else:
+        print("*** There is ONE seat left!! ***")
+
+    # Get details...
 
     # Get name (can't be blank)
     name = not_blank("Name: ")
+    count += 1
+    print()
+
+if count == MAX_TICKETS:
+    print("You have sold all the available tickets!")
+else:
+    print("You have sold {} tickets.  \n"
+          "There are {} places still available"
+          .format(count, MAX_TICKETS - count))
+
+
+
 
     # Get age (between 12 and 130
 
